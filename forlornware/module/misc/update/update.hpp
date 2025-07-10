3ec2e78ca31520380efb37e7df7ff014db3c1bfc
ForlornWindow46 <92132429+ForlornWindow46@users.noreply.github.com>
2025-07-10 21:50:06 +1000

    e

--- forlornware/module/misc/update/update.hpp
+++ forlornware/module/misc/update/update.hpp
@@ -73,28 +73,24 @@ namespace roblox
 #define LUAU_SHUFFLE8(s, a1, a2, a3, a4, a5, a6, a7, a8) a4 s a3 s a2 s a7 s a6 s a5 s a8 s a1
 #define LUAU_SHUFFLE9(s, a1, a2, a3, a4, a5, a6, a7, a8, a9) a1 s a5 s a3 s a2 s a4 s a9 s a8 s a7 s a6
 
-#define UDATA_META_ENC        VMValue1
-#define CLOSURE_CONT_ENC      VMValue1
-#define PROTO_DEBUGISN_ENC    VMValue1
-
-#define PROTO_DEBUGNAME_ENC   VMValue2
-#define TSTRING_HASH_ENC      VMValue2
-
-
-#define CLOSURE_DEBUGNAME_ENC VMValue3
-#define PROTO_MEMBER2_ENC     VMValue3
-#define PROTO_TYPEINFO_ENC    VMValue4
-#define LSTATE_STACKSIZE_ENC  VMValue4
-
-
-
-
-#define TSTRING_LEN_ENC   VMValue0
+#define TSTRING_LEN_ENC VMValue0
 #define GSTATE_TTNAME_ENC VMValue0
 #define GSTATE_TMNAME_ENC VMValue0
 #define PROTO_MEMBER1_ENC VMValue0
-#define CLOSURE_FUNC_ENC  VMValue0
+#define CLOSURE_FUNC_ENC VMValue0
 #define LSTATE_GLOBAL_ENC VMValue0
+#define TABLE_MEMBER_ENC VMValue0
+#define TABLE_META_ENC VMValue0
 
-#define TABLE_MEMBER_ENC  VMValue0
-#define TABLE_META_ENC    VMValue0
+#define UDATA_META_ENC VMValue1
+#define CLOSURE_CONT_ENC VMValue1
+#define PROTO_DEBUGISN_ENC VMValue1
+
+#define PROTO_DEBUGNAME_ENC VMValue2
+#define TSTRING_HASH_ENC VMValue2
+
+#define CLOSURE_DEBUGNAME_ENC VMValue3
+#define PROTO_MEMBER2_ENC VMValue3
+
+#define PROTO_TYPEINFO_ENC VMValue4
+#define LSTATE_STACKSIZE_ENC VMValue4
